1d567cd64b50b730daa9c06cef1218a205e2567d
yoonyeoul37 <info@knexm.com>
2026-09-22 14:19:00 +0900

    라디오/자연소리 화면 종료 인삿말 음성 파일 버전(v3) 통일

--- lib/screens/nature_sound_detail_screen.dart
+++ lib/screens/nature_sound_detail_screen.dart
@@ -320,7 +320,7 @@ class _NatureSoundDetailScreenState extends State<NatureSoundDetailScreen> {
     late final String farewellAsset;
     switch (langCode) {
       case 'ko':
-        farewellAsset = 'assets/farewell_ko_v2.mp3';
+        farewellAsset = 'assets/farewell_ko_v3.mp3';
         break;
       case 'ja':
         farewellAsset = 'assets/farewell_ja.mp3';
--- lib/screens/radio_player_screen.dart
+++ lib/screens/radio_player_screen.dart
@@ -68,7 +68,7 @@ class _RadioPlayerScreenState extends State<RadioPlayerScreen>
     switch (langCode) {
       case 'ko':
         smallText = '파란소리와 함께한 시간,\n즐거우셨나요?\n\n언제든 다시 찾아오시면,\n좋은 소리로 맞아드릴게요.\n안녕히 가세요!';
-        farewellAsset = 'assets/farewell_ko_v2.mp3';
+        farewellAsset = 'assets/farewell_ko_v3.mp3';
         break;
       case 'ja':
         smallText = 'Paransoriと過ごした時間、\n楽しんでいただけましたか?\n\nいつでもまた遊びに来てください、\n素敵な音でお迎えします。\nまた会いましょう!';
